c082ef0552a7e02802333dca3a762b80f019c67b
Spencer Newton <sn.newton92@gmail.com>
2019-05-06 20:21:38 +0100

    stream checker middleware functionality

--- src/routes/stream-count-checker.js
+++ src/routes/stream-count-checker.js
@@ -1,3 +1,41 @@
-export default (req, res) => {
-    return res.send(true);
+import users from '../data/users';
+
+const userCheck = (id) => {
+    // Would send request to check database, but since it is out of the scope of this test we will just check the static data file
+    return users.find((user) => {
+        console.log(user, user.id === id);
+        return user.id === id;
+    });
+};
+
+export default async (req, res) => {
+    const userId = req.params.userId;
+
+    if (!userId) {
+        return res.send({
+            limitReached: true,
+            error: 'User ID Not Provided'
+        });
+    }
+
+    try {
+        const user = await userCheck(userId);
+
+        if (!user) {
+            return res.send({
+                limitReached: true,
+                error: 'User Not Found'
+            });
+        }
+
+        return res.send({
+            limitReached: user.currentStreams > process.env.BASIC_USER_MAX_STREAMS
+        });
+    }
+    catch (e) {
+        return res.send({
+            limitReached: true,
+            error: `Error Checking User Info: ${e}`
+        });
+    }
 }
