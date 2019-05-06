4f8586bbf42c1ea0365da86cf47afa28a6db6bce
Spencer Newton <sn.newton92@gmail.com>
2019-05-06 19:30:50 +0100

    eslint config

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.listen(process.env.PORT, () => {
     console.log('Running STREAM-LIMITER App...');
     console.log(`Max Streams for Basic User: ${process.env.BASIC_USER_MAX_STREAMS}`); 
